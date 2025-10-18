71d8f743ac1a04aba26d080c1380cfb6a01449b0
Ivan C Myrvold <ivan@myrvold.org>
2025-10-18 17:49:25 +0200

    Initial Commit

---
+++ CocoCashuApp/CocoCashuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocoCashuAppApp.swift
+//  CocoCashuApp
+//
+//  Created by Ivan C Myrvold on 18/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct CocoCashuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CocoCashuAppTests/CocoCashuAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  CocoCashuAppTests.swift
+//  CocoCashuAppTests
+//
+//  Created by Ivan C Myrvold on 18/10/2025.
+//
+
+import Testing
+
+struct CocoCashuAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
